a8d2ff9ccd0edac29d8b289bd1711549ca5b8ac7
Kevin Barrera <kev97barrera@gmail.com>
2017-10-31 12:10:02 -0600

    Agregadas routes de proyecto y criterio

---
+++ tests/unit/routes/criterio-route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:criterio-route', 'Unit | Route | criterio route', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
